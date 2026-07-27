3e85035f815324e7bc9ac6c8398464e1695016db
Harshita <guptaharshita8126@gmail.com>
2026-07-27 16:49:47 +0530

    Bubble Sort

---
+++ Sorting/bubblesort.java
@@ -0,0 +1,26 @@
+//Implementation of bubble sort 
+
+// swaps the adjacent element and the largest element is placed to the last of array .
+import java.util.*;
+class bubblesort{
+    
+    public static void bubbleSort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={5,3,8,4,2};
+        bubbleSort(arr);
+        
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
